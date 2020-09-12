3c63b1277c41a19e6f69fa9311e6a0de8a83c039
MasterDjefer <kostia210@gmail.com>
2020-09-12 13:29:05 +0300

    added states

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,23 +3,75 @@
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent)
 {
-    MenuButton* mButton1 = new MenuButton;
-    mButton1->setText("Jager");
-    MenuButton* mButton2 = new MenuButton;
-    mButton2->setText("Becher");
-    MenuButton* mButton3 = new MenuButton;
-    mButton3->setText("Sadochok");
-
-    QVBoxLayout* mMainLayout = new QVBoxLayout;
-    mMainLayout->addWidget(mButton1);
-    mMainLayout->addWidget(mButton2);
-    mMainLayout->addWidget(mButton3);
-    mMainLayout->setAlignment(Qt::AlignHCenter);
-
-    this->setLayout(mMainLayout);
+    mStackedLayout = new QStackedLayout;
+    this->setLayout(mStackedLayout);
+    initMainMenu();
+    initGameModeMenu();
+    initStates();
 }
 
 MainWindow::~MainWindow()
 {
 }
 
+void MainWindow::initMainMenu()
+{
+    mMainMenu = new QVBoxLayout;
+
+    mButtonNewGame = new MenuButton;
+    mButtonNewGame->setText(mButtonTitles[NewGame]);
+    MenuButton* mButtonSettings = new MenuButton;
+    mButtonSettings->setText(mButtonTitles[Settings]);
+    MenuButton* mButtonAbout = new MenuButton;
+    mButtonAbout->setText(mButtonTitles[About]);
+    MenuButton* mButtonExit = new MenuButton;
+    mButtonExit->setText(mButtonTitles[Exit]);
+
+    mMainMenu->addWidget(mButtonNewGame);
+    mMainMenu->addWidget(mButtonSettings);
+    mMainMenu->addWidget(mButtonAbout);
+    mMainMenu->addWidget(mButtonExit);
+
+    mMainMenu->setAlignment(Qt::AlignHCenter);
+
+    QWidget *w = new QWidget;
+    w->setLayout(mMainMenu);
+    mStackedLayout->addWidget(w);
+}
+
+void MainWindow::initGameModeMenu()
+{
+    mGameModeMenu = new QVBoxLayout;
+
+    MenuButton* mButtonCreateGame = new MenuButton;
+    mButtonCreateGame->setText(mButtonTitles[CreateGame]);
+    MenuButton* mButtonConnectToGame = new MenuButton;
+    mButtonConnectToGame->setText(mButtonTitles[ConnectToGame]);
+
+    mGameModeMenu->addWidget(mButtonCreateGame);
+    mGameModeMenu->addWidget(mButtonConnectToGame);
+
+    mGameModeMenu->setAlignment(Qt::AlignHCenter);
+
+    QWidget *w = new QWidget;
+    w->setLayout(mGameModeMenu);
+    mStackedLayout->addWidget(w);
+}
+
+void MainWindow::initStates()
+{
+    mStateMachine = new QStateMachine(this);
+
+    QState *state1 = new QState(mStateMachine);
+    QState *state2 = new QState(mStateMachine);
+
+    state1->assignProperty(mStackedLayout, "currentIndex", 0);
+    state1->addTransition(mButtonNewGame, &MenuButton::clicked, state2);
+
+    state2->assignProperty(mStackedLayout, "currentIndex", 1);
+
+    mStateMachine->setInitialState(state1);
+    mStateMachine->start();
+}
+
+const char* MainWindow::mButtonTitles[] = {"New game", "Settings", "About", "Exit", "Create game", "Connect to game"};
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,10 @@
 #include <QVBoxLayout>
 #include <QWidget>
 #include <QGroupBox>
+#include <QStackedLayout>
+
+#include <QState>
+#include <QStateMachine>
 
 #include "menubutton.h"
 
@@ -12,10 +16,36 @@ class MainWindow : public QWidget
 {
     Q_OBJECT
 
+    enum
+    {
+        NewGame = 0,
+        Settings,
+        About,
+        Exit,
+        CreateGame,
+        ConnectToGame
+    };
+
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
+    void initMainMenu();
+    void initGameModeMenu();
+    void initStates();
+
+private:
+    QStackedLayout *mStackedLayout;
+
+    QVBoxLayout* mMainMenu;
+    MenuButton* mButtonNewGame;
+
+    QVBoxLayout* mGameModeMenu;
+    QVBoxLayout* mSettingsMenu;
+
+    QStateMachine* mStateMachine;
+
+    static const char* mButtonTitles[];
 };
 #endif // WIDGET_H
